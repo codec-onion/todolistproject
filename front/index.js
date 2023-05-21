411e0df77227389bd89122e2268040710e834cf3
codec-onion <bonnetmatthieu86@gmail.com>
2023-05-21 20:50:00 +0200

    only 1 list at a time and possibility to remove task validation

--- front/index.js
+++ front/index.js
@@ -1,11 +1,22 @@
 const inputToDoListName = document.querySelector(".createtodolist__input")
 const buttonCreate = document.querySelector(".createtodolist__button")
+const formCreateList = document.querySelector(".createtodolist__form")
 
-
-buttonCreate.addEventListener("click", function (event) {
-    createToDoList()
-    createElement()
+formCreateList.addEventListener("submit", function (event) {
+    event.preventDefault()
+    if(inputToDoListName.value === "") {
+        return false
+    } else if (!document.querySelector(".todolist")) {
+        createToDoList()
+        createElement()
+    } else {
+        const warning = document.createElement("p")
+        warning.textContent = "Une liste est déjà en cours."
+        const createSection = document.querySelector(".createtodolist")
+        createSection.appendChild(warning)
+    }
 })
+
 
 function createToDoList() {
     const main = document.querySelector("main")
@@ -16,6 +27,7 @@ function createToDoList() {
     const title = document.createElement("h2")
     title.className = "todolist__title"
     title.textContent = inputToDoListName.value
+    inputToDoListName.value = ""
 
     const div = document.createElement("div")
     div.className = "todolist__body"
@@ -23,17 +35,20 @@ function createToDoList() {
     const ul = document.createElement("ul")
     ul.className = "todolist__body-list"
 
+    const form = document.createElement("form")
+    form.setAttribute("action", "#")
+    form.className = "todolist__body-form"
+
     const label = document.createElement("label")
     label.className = "todolist__body-label"
     label.setAttribute("for", "tache")
-    label.textContent = "Nouvelle tâche"
+    label.textContent = "Nouvelle tâche: "
 
     const input = document.createElement("input")
     input.setAttribute("type", "text")
     input.className = "todolist__body-input"
     input.setAttribute("id", "tache")
     input.setAttribute("name", "tache")
-
 
     const button = document.createElement("button")
     button.className = "todolist__body-button"
@@ -43,26 +58,41 @@ function createToDoList() {
     section.appendChild(title)
     section.appendChild(div)
     div.appendChild(ul)
-    div.appendChild(label)
-    div.appendChild(input)
-    div.appendChild(button)
+    div.appendChild(form)
+    form.appendChild(label)
+    form.appendChild(input)
+    form.appendChild(button)
 }
 
-
-
 function createElement() {
-    const buttonAddElement = document.querySelector(".todolist__body-button")
-    buttonAddElement.addEventListener("click", function (event) {
-        const ul = document.querySelector(".todolist__body-list")
-        const input = document.querySelector(".todolist__body-input")
+    const formCreateThing = document.querySelector(".todolist__body-form")
+    const ul = document.querySelector(".todolist__body-list")
+    const input = document.querySelector(".todolist__body-input")
+    formCreateThing.addEventListener("submit", function (event) {
+        event.preventDefault()
+        if (input.value === "") {
+            return false
+        } else {
+            const li = document.createElement("li")
+            li.textContent = `${input.value}`
+            input.value = ""
+            validateElement(li)
 
-        const li = document.createElement("li")
-        li.textContent = input.value
-
-        ul.appendChild(li)
+            ul.appendChild(li)
+        }
     })
 }
 
-// createToDoListButton()
-//     .then(() => createElement())
-//     .catch((error) => {error})
+function validateElement(li) {
+    let isDone
+    const valid = " VALIDÉE!"
+    li.addEventListener("click", function (event) {
+        if (!isDone) {
+            li.textContent += valid
+            isDone = true
+        } else if (isDone) {
+            li.textContent = li.textContent.split(`${valid}`)[0]
+            isDone = false
+        }
+    })
+}
